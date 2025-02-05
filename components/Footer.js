3332434326a7b9de24d0a7ce62e0e1d2fd289068
Bloomer004 <ishaguptavnsv@gmail.com>
2025-02-05 16:14:01 +0530

    added a copyright section

--- components/Footer.js
+++ components/Footer.js
@@ -5,6 +5,7 @@ const Footer = () => {
   return (
     <div>
       This is footer component. My name is isha. My name is YashRajvashi
+      <div className='bg-red-800'>Copyright &bull; Isha</div>
     </div>
   )
 }
