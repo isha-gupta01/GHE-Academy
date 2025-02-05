89f13e50620f17d224578b606349fcf6230ea2db
Bloomer004 <ishaguptavnsv@gmail.com>
2025-02-06 01:12:58 +0530

    Hero Page made half

--- Sections/Hero.jsx
+++ Sections/Hero.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import Image from 'next/image'
-import SchoolImage from "@/assets/images/ghe1.png"
 
-const Hero = () => {
+const Hero = ({image,title,subBody}) => {
     return (
         <div>
-            <div className='relative w-full h-[80vh]'>
-                <Image src={SchoolImage} alt='School Image' fill className='object-cover' />
+            <div className={`relative w-full h-[73vh]`}>
+                <Image src={image} alt='School Image' fill className='object-cover' />
             </div>
             <div className="absolute inset-0 bottom-[2.9rem] top-[8.87rem] flex flex-col items-center justify-center text-white bg-black/50">
-                <h1 className="text-3xl md:text-4xl lg:text-6xl font-bold">Welcome</h1>
-                <p className="text-base md:text-lg lg:text-xl mt-2">Your journey starts here</p>
+                <h1 className="text-3xl md:text-4xl lg:text-6xl font-bold">{title}</h1>
+                <p className="text-base md:text-lg lg:text-xl mt-2">{subBody}</p>
             </div>
 
         </div>
--- app/page.js
+++ app/page.js
@@ -2,13 +2,14 @@ import Image from "next/image";
 import Navbar from "@/Sections/Navbar";
 import Header from "@/Sections/Header";
 import Hero from "@/Sections/Hero";
+import SchoolImage from "@/assets/images/ghe1.png"
 
 export default function Home() {
   return (
     <>
       <Header />
       <Navbar />
-      <Hero />
+      <Hero image={SchoolImage} title="Welcome to" subBody="Green Heaven English Academy"/>
     </>
   )
 }
