6e27d8c102ac23a899cdb7cc9f12161700592670
Bloomer004 <ishaguptavnsv@gmail.com>
2025-02-05 15:54:59 +0530

    checking the git working

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      this is Footer components which we are going to use.
+      this is Footer components which we are going to use.Hello User
     </div>
   )
 }
