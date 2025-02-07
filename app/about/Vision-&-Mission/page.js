11074888d71d374f0cc2b052f39535c434b0460b
yash-rajvanshi <yash526452@gmail.com>
2025-02-07 10:30:55 +0530

    changes in visoin

--- app/about/Vision-&-Mission/page.js
+++ app/about/Vision-&-Mission/page.js
@@ -3,6 +3,8 @@ import Navbar from '@/Sections/Navbar'
 import Header from '@/Sections/Header'
 import Hero from '@/Sections/Hero'
 import SchoolImage from "@/assets/images/ghe1.png"
+import Footer from '@/Sections/Footer'
+
 
 const VisionMission = () => {
   return (
@@ -12,18 +14,19 @@ const VisionMission = () => {
         <Navbar />
         <Hero image={SchoolImage} title="Vision & Mission" subBody="Excellence in every Student" height={70} />
         <div className='w-[80%] mr-auto ml-auto'>
-          <h1 className='text-center mt-10 font-extralight text-[3rem] text-[#76B947]'>Vision and Mission</h1>
+          <h1 className='text-center mt-10 font-extralight text-[3rem] text-[#76B947]'>Vision & Mission</h1>
           <p className='text-lg'>Green Heaven English Academy provides a balanced mix of academic excellence and personal development, creating opportunities for students to thrive in a nurturing environment and reach their full potential.</p>
-          <div className='border rounded-3xl bg-[#76b9478d] mt-10 border-green-500 p-5'>
+          <div className='shadow-[0px_0px_12px_0px_rgba(0,_0,_0,_0.35)] rounded-3xl bg-[#76b9478d] mt-10 p-5'>
             <h1 className='text-center mb-2 text-[#2F5233] font-bold text-[2rem]'>Vision</h1>
-            <h2 className='text-2xl text-[#2F5233]'>Our vision is to create a nurturing and inclusive learning environment where every student can unlock their full potential, embrace lifelong learning, and become responsible, compassionate global citizens. We aim to inspire curiosity, foster creativity, and develop the skills needed to excel in a rapidly changing world.</h2>
+            <h2 className='text-xl text-[#2F5233]'>Our vision is to create a nurturing and inclusive learning environment where every student can unlock their full potential, embrace lifelong learning, and become responsible, compassionate global citizens. We aim to inspire curiosity, foster creativity, and develop the skills needed to excel in a rapidly changing world. Through strong values and a student-centered approach, we strive to build a foundation for lifelong success.</h2>
           </div>
-          <div className=' border rounded-3xl bg-[#76b9478d] mt-10 border-green-500 p-5'>
+          <div className='shadow-[0px_0px_12px_0px_rgba(0,_0,_0,_0.35)] rounded-3xl bg-[#76b9478d] my-10 p-5'>
             <h1 className='text-center mb-2 text-[#2F5233] font-bold text-[2rem]'>Mission</h1>
-            <h2 className='text-2xl text-[#2F5233]'>Our mission is to provide a high-quality, holistic education that empowers students to grow intellectually, socially, and emotionally. We are committed to fostering a culture of respect, discipline, and collaboration, where each child is encouraged to strive for excellence, develop critical thinking, and contribute positively to society. Through innovative teaching methods and a supportive community, we prepare our students for a successful future.</h2>
+            <h2 className='text-xl text-[#2F5233]'>Our mission is to provide a high-quality, holistic education that empowers students to grow intellectually, socially, and emotionally. We are committed to fostering a culture of respect, discipline, and collaboration, where each child is encouraged to strive for excellence, develop critical thinking, and contribute positively to society. Through innovative teaching methods and a supportive community, we prepare our students for a successful future.</h2>
           </div>
         </div>
       </div>
+      <Footer/>
     </>
   )
 }
