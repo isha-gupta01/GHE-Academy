0557c11430ebd2ffc6bba733f4cf7105f75f9be1
Bloomer004 <ishaguptavnsv@gmail.com>
2025-02-09 21:54:31 +0530

    facilities completed

--- Sections/Header.jsx
+++ Sections/Header.jsx
@@ -9,7 +9,7 @@ import { FaFacebook, FaWhatsapp } from 'react-icons/fa'
 
 const Header = () => {
     return (
-        <div className='sm:block py-1  sm:justify-between sm:px-7 lg:block md:block'>
+        <div className='sm:block py-1  sm:justify-between sm:px-7 lg:block md:block pt-[0.5rem]'>
             <div className=' flex md:h-[2rem] bg-white justify-between gap-8  items-center'>
                 <div className='uppercase sm:text-xs gap-4 flex items-center'>
                     <Link href="mailto:greenheavenacademy2011@gmail.com"><Image src={Email} alt='email' width={30} height={30} /></Link>
