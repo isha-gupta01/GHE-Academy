0968cc87cebd1de9f7aa0260adcd0fa743fe7cac
Bloomer004 <ishaguptavnsv@gmail.com>
2025-02-09 14:06:26 +0530

    working on popup

--- Sections/Header.jsx
+++ Sections/Header.jsx
@@ -8,15 +8,16 @@ import Image from 'next/image'
 const Header = () => {
     return (
         <div className='sm:block py-1  sm:justify-between sm:px-7 lg:block md:block'>
-            <div className=' flex  md:h-[2rem] bg-white justify-between gap-8  items-center'>
+            <div className=' flex md:h-[2rem] bg-white justify-between gap-8  items-center'>
                 <div className='uppercase sm:text-xs gap-4 flex items-center'>
                     <Link href="mailto:greenheavenacademy2011@gmail.com"><Image src={Email} alt='email' width={30} height={30} /></Link>
                     <Link href="https://wa.me/91" target="_blank" rel="noopener noreferrer">
                         <Image src={Whatsapp} alt='email' width={30} height={30} />
                     </Link>
                 </div>
-                <div className='font-serif font-bold '><Link href="/LoginPage">Login</Link></div>
-            </div>
+                <div className='border border-black h-[2rem] rounded-full shadow-[0px_0px_14px_2px_rgba(0,_0,_0,_0.1)] flex items-center px-3  gap-4 text-sm '><Link href="/LoginPage">Login</Link></div>
+            </div>                
+            
         </div>
     )
 }
--- components/PopUp.js
+++ components/PopUp.js
@@ -3,7 +3,8 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import Cross from "@/assets/icons/icons8-cross-48.png";
-import SchoolLogo from "@/assets/images/logo.png";
+import Alert from "@/assets/icons/alert1.jpg";
+import SignUp from "@/assets/icons/signup.png"
 
 const PopUp = () => {
   const [isPopUpOpen, setIsPopUpOpen] = useState(false);
@@ -31,16 +32,17 @@ const PopUp = () => {
             exit={{ scale: 0.8, opacity: 0 }}
             className="fixed inset-0 flex justify-center items-center z-50"
           >
-              <div className="bg-white pt-4 h-[30rem] w-[25rem] rounded-2xl flex justify-center flex-col gap-20 items-center">
-            <div className="flex flex-col justify-center">
-                <Image src={SchoolLogo} alt="icon" width={200} height={200}/>
-                </div>
-              <div className="text-white w-full bg-yellow-500 h-[13rem] rounded-b-2xl flex flex-col items-center justify-center">
-                <span>Updates</span>
-                <span>Admissions Open 2025-26</span>
-                <button>Register Now!</button>
+            <div className="bg-white pt-4 md:h-[35rem] md:w-[25rem] sm:h-[35rem] sm:w-[22rem] rounded-2xl flex justify-center flex-col gap-4 items-center">
+              <button onClick={closePopup} className=" z-10 md:pl-[22rem] sm:pl-[18rem]"><Image src={Cross} alt="cross" width={30} height={30}/></button>
+              <div className="flex flex-col justify-center">
+                <Image src={Alert} alt="icon" width={250} height={300} />
               </div>
-              
+              <div className="text-white w-full gap-5 bg-yellow-500 h-[13rem] sm:h-[15rem] rounded-b-2xl flex flex-col items-center justify-center">
+                <span className="text-3xl  underline text-gray-900 ">Updates</span>
+                <span className="text-2xl  ">Admissions Open 2025-26</span>
+                <button className="border border-black rounded-full shadow-[0px_0px_14px_2px_rgba(0,_0,_0,_0.1)] flex items-center bg-emerald-500 px-4 py-2 gap-4 text-xl"><span>Register Now! </span><span><Image src={SignUp} alt="register" width={30} height={30}/></span></button>
+              </div>
+
             </div>
           </motion.div>
         </>
