065c655382ad73ca1f09858e2b93e7c70815307b
yash-rajvanshi <yash526452@gmail.com>
2025-02-10 15:47:11 +0530

    changes in header, staff wagera

--- app/Contact/page.js
+++ app/Contact/page.js
@@ -26,7 +26,7 @@ const ContactPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     
-    const response = await fetch("http://localhost:5000/send-email", {
+    const response = await fetch("http://localhost:5001/send-email", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
