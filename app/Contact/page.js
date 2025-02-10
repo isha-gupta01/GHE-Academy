c5af771bc93e9f3464398662735a0b0d2bf4acab
yash-rajvanshi <yash526452@gmail.com>
2025-02-10 20:51:38 +0530

    changes in header, staff wagera

--- app/Contact/page.js
+++ app/Contact/page.js
@@ -77,7 +77,7 @@ const ContactPage = () => {
     <Header/>
     <Navbar />
     {/* <Hero image={SchoolImage} title="" subBody="" height={70}/> */}
-    <div className="min-h-screen  flex justify-center items-center bg-[#5E9538] p-4">
+    <div className="min-h-screen  flex justify-center items-center bg-slate-600 p-4">
       <div className="w-full sm:mt-[10rem] lg:mt-[8rem] max-w-4xl bg-gradient-to-br from-gray-900 to-gray-800 text-white rounded-lg shadow-xl p-6 md:p-10 flex flex-col md:flex-row">
         {/* Left Section - Form */}
         <div className="w-full md:w-2/3">
