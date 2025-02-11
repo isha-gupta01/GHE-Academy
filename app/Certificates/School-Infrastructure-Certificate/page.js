031f32d61482aa48cb1f7d996b8e6b74024987bf
yash-rajvanshi <yash526452@gmail.com>
2025-02-11 21:39:15 +0530

    changes in header, staff wagera

--- app/Certificates/School-Infrastructure-Certificate/page.js
+++ app/Certificates/School-Infrastructure-Certificate/page.js
@@ -5,6 +5,8 @@ import Footer from '@/Sections/Footer'
 import Hero from '@/Sections/Hero'
 import SchoolImage from "@/assets/images/certification1.png"
 import Updates from '@/components/Updates'
+import { Quando } from 'next/font/google';
+import QuickLinks from '@/components/QuickLinks';
 
 const ImageGallery = () => {
   const images = [
@@ -19,13 +21,17 @@ const ImageGallery = () => {
     <Updates/>
     <Navbar/>
     <Hero image={SchoolImage} title="" subBody="School infrastructure certificate" height={70}/>
-    <div className="min-h-screen flex flex-col items-center p-4">
-        <h1 className="text-3xl mt-8 font-bold text-green-700">School infrastructure Certicifate</h1>
-      <div className="w-full mt-12 shadow-[0px_0px_5px_2px_rgba(0,_0,_0,_0.1)] p-3 rounded-lg max-w-3xl flex flex-col items-center gap-4">
-        {images.map((src, index) => (
-          <img key={index} src={src} alt={`Image ${index + 1}`} className="w-full rounded-lg shadow-md" />
-        ))}
-      </div>
+    <div className="flex md:flex-row lg:m-20 md:gap-32 sm:flex-col">
+      <div className="min-h-screen flex flex-col items-center p-4">
+          <h1 className="text-3xl mt-8 font-bold text-green-700">School infrastructure Certicifate</h1>
+        <div className="w-full mt-12 shadow-[0px_0px_5px_2px_rgba(0,_0,_0,_0.1)] p-3 rounded-lg max-w-3xl flex flex-col items-center gap-4">
+          {images.map((src, index) => (
+            <img key={index} src={src} alt={`Image ${index + 1}`} className="w-full rounded-lg shadow-md" />
+          ))}
+        </div>
+
+    </div>
+    <QuickLinks/>
     </div>
     <Footer/>
     </>
--- app/Certificates/Self-Certification/page.js
+++ app/Certificates/Self-Certification/page.js
@@ -5,6 +5,7 @@ import Footer from '@/Sections/Footer'
 import Hero from '@/Sections/Hero'
 import SchoolImage from "@/assets/images/certification1.png"
 import Updates from '@/components/Updates';
+import QuickLinks from '@/components/QuickLinks';
 
 const ImageGallery = () => {
   const images = [
@@ -16,13 +17,17 @@ const ImageGallery = () => {
     <Updates/>
     <Navbar/>
     <Hero image={SchoolImage} title="" subBody="Self certification" height={70}/>
-    <div className="min-h-screen flex flex-col items-center p-4">
-        <h1 className="text-3xl mt-8 font-bold text-green-700">Self certification</h1>
-      <div className="w-full mt-12 shadow-[0px_0px_5px_2px_rgba(0,_0,_0,_0.1)] p-3 rounded-lg max-w-3xl flex flex-col items-center gap-4">
-        {images.map((src, index) => (
-          <img key={index} src={src} alt={`Image ${index + 1}`} className="w-full rounded-lg shadow-md" />
-        ))}
-      </div>
+    <div className="flex md:flex-row lg:m-20 md:gap-32 sm:flex-col">
+      <div className="min-h-screen flex flex-col items-center p-4">
+          <h1 className="text-3xl mt-8 font-bold text-green-700">Self certification</h1>
+        <div className="w-full mt-12 shadow-[0px_0px_5px_2px_rgba(0,_0,_0,_0.1)] p-3 rounded-lg max-w-3xl flex flex-col items-center gap-4">
+          {images.map((src, index) => (
+            <img key={index} src={src} alt={`Image ${index + 1}`} className="w-full rounded-lg shadow-md" />
+          ))}
+        </div>
+
+    </div>
+    <QuickLinks/>
     </div>
     <Footer/>
     </>
