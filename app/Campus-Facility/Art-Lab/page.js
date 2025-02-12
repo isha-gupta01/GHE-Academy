a09df65942e17a797e76f7e83ad70b7b0fa31ece
yash-rajvanshi <yash526452@gmail.com>
2025-02-12 19:09:57 +0530

    changed section spell

--- app/Campus-Facility/Art-Lab/page.js
+++ app/Campus-Facility/Art-Lab/page.js
@@ -15,7 +15,7 @@ const ArtLab = () => {
         <Updates/>
         <Navbar />
         <Hero image={SchoolImage} title="Art Lab" subBody="" height={70} />
-        <div className='flex-col flex md:h-[80vh] sm:h-[100vh]'>
+        <div className='flex-col flex md:h-[100vh] sm:h-[100vh]'>
           <div><h1 className='font-extralight text-[3rem] mt-4 mb-6 lg:ml-14 sm:mx-4'>Art Lab</h1></div>
           <div className='sm:flex-col mx-5 lg:flex-row md:flex-row flex' >
             <Image className='lg:rounded-lg sm:h-auto  sm:w-auto  lg:ml-8 lg:mr-8 lg:h-[400px] lg:w-[400px] md:h-auto md:w-[400px]' src={Art} alt='art' />
