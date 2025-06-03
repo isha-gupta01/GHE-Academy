6c73aedb54f829d0e7127c6058af66ffa7734c76
yash-rajvanshi <yash526452@gmail.com>
2025-06-03 18:46:29 +0530

    self certification added

--- app/Certificates/Self-Certification/page.js
+++ app/Certificates/Self-Certification/page.js
@@ -11,7 +11,7 @@ import Image from 'next/image';
 
 const ImageGallery = () => {
   const images = [
-    "/certificates/certification.jpg",
+    "/certificates/certification.png",
   ]; // Replace with your actual image paths
 
   return (
